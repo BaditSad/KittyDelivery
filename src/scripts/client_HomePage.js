029d890693f6e84bfdeebf540062efc5100b8500
KwiiKss <lrent.kevin@gmail.com>
2024-06-17 14:27:36 +0200

    fix error parrainage v2

--- src/scripts/client_HomePage.js
+++ src/scripts/client_HomePage.js
@@ -1,39 +1,56 @@
 import client_HomePage from '@/components/client_HomePage.vue'
-import emailjs from 'emailjs-com';
 
 export default {
   name: 'client_HomePage',
-  mounted() {
-    emailjs.init("qW1Ea3nn6YSeahlbA");
-
-    const button = this.$el.querySelector(".referral button");
-    const emailInput = this.$el.querySelector(".referral .text");
-
-    button.addEventListener("click", () => {
-      const toEmail = emailInput.value;
-      if (this.validateEmail(toEmail)) {
-        this.sendReferralEmail(toEmail);
-      } else {
-        alert("Veuillez entrer une adresse email valide.");
-      }
-    });
-  },
-  methods: {
-    validateEmail(email) {
-      const re = /\S+@\S+\.\S+/;
-      return re.test(email);
-    },
-    sendReferralEmail(toEmail) {
-      emailjs.send("service_lgpn366", "template_8a8z0cg", {
-        to_email: toEmail
-      })
-      .then(response => {
-        console.log('SUCCESS!', response.status, response.text);
-        alert("Email de parrainage envoyé avec succès !");
-      }, error => {
-        console.log('FAILED...', error);
-        alert("Erreur lors de l'envoi de l'email de parrainage.");
-      });
-    }
+  components: {
+    client_HomePage
   }
 };
+
+/* eslint-disable */
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.querySelector(".referral button");
+  const emailInput = document.querySelector(".referral .text");
+
+  console.log("Button:", button);
+  console.log("Email Input:", emailInput);
+
+  if (button && emailInput) {
+    const script = document.createElement("script");
+    script.src = "https://cdn.emailjs.com/dist/email.min.js";
+    script.onload = () => {
+      emailjs.init("qW1Ea3nn6YSeahlbA");
+
+      button.addEventListener("click", function() {
+        const toEmail = emailInput.value;
+        if (validateEmail(toEmail)) {
+          sendReferralEmail(toEmail);
+        } else {
+          alert("Veuillez entrer une adresse email valide.");
+        }
+      });
+
+      function validateEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+      }
+
+      function sendReferralEmail(toEmail) {
+        emailjs.send("service_lgpn366", "template_8a8z0cg", {
+          to_email: toEmail
+        })
+        .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          alert("Email de parrainage envoyé avec succès !");
+        }, function(error) {
+          console.log('FAILED...', error);
+          alert("Erreur lors de l'envoi de l'email de parrainage.");
+        });
+      }
+    };
+    document.head.appendChild(script);
+  } else {
+    console.error("Button or Email Input not found in the DOM.");
+  }
+});
+/* eslint-enable */
