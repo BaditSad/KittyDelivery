de02125ea794c50e54304dd3f141a207ded9d056
KwiiKss <lrent.kevin@gmail.com>
2024-06-18 17:31:34 +0200

    update pagination article

--- src/services/HandlerGetArticles.js
+++ src/services/HandlerGetArticles.js
@@ -1,9 +1,8 @@
 import apiGateway from "../../axios.config";
 
-export const getArticles = async (restaurantId) => {
-  restaurantId = 1;
+export const getArticles = async (page, limit) => {
   try {
-    const response = await apiGateway.get(`/mc_article/${restaurantId}`);
+    const response = await apiGateway.get(`/mc_article`, { params: { page, limit } });
     return response.data;
   } catch (error) {
     console.error("Erreur lors de la récupération des articles:", error);
