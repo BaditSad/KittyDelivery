25dead9b6014dedc34898fd834c8ec549e19ed3a
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-20 12:23:35 +0200

    feature(add https)

--- axios.config.js
+++ axios.config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://localhost:3000/api";
 const TOKEN_ACCESS_KEY = "accessToken";
 
 const apiGateway = axios.create({
